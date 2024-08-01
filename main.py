7ef35c4cd560a2db9b5ad10335698d12e9bf06d1
Luis1077 <149336352+Luis1077@users.noreply.github.com>
2024-08-01 12:01:46 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from tqdm import tqdm
 import nltk
 
-
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
 # Load the CSV file with file path
-df = pd.read_csv()
+df = pd.read_csv("/Users/lgsan/Downloads/Amazon_Reviews_Exporter_B00EZP4S86_2024_7_4_11_48_45.csv")
 
 # Sentiment Analyzer Initialization
 sia = SentimentIntensityAnalyzer()
@@ -23,7 +22,6 @@ res = {}
 for i, row in tqdm(df.iterrows(), total=len(df)):
     text = str(row['content'])  # Convert the title to string
     myid = row['id']
-
 
     if text and text != 'nan':
         res[myid] = sia.polarity_scores(text)
@@ -37,18 +35,19 @@ vaders = vaders.reset_index().rename(columns={'index': 'id'})
 # Merging of the sentiment scores with the original DataFrame
 vaders = vaders.merge(df, how='left', on='id')
 
-# Calculate and print the average compound score
-average_compound = vaders['compound'].mean()
-print(f"Average Compound Score: {average_compound}")
-
 # Compound Score Display
 print(res)
 print(vaders[['id', 'compound']])
+
+# Calculate and print the average compound score
+average_compound = vaders['compound'].mean()
+print(f"Average Compound Score: {average_compound}")
 
 # Plot for count of reviews by stars
 ax = df['rating'].value_counts().sort_index().plot(kind='bar', title='Count of Reviews by Stars', figsize=(10, 5))
 plt.xlabel('Star Rating')
 plt.ylabel('Count')
 plt.show()
+
 
 
